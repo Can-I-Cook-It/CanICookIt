e0de8b308550a3f77cdd7adb96e0683aee4c1f5b
tfrosendo <tfrosendo@users.noreply.github.com>
2017-10-25 14:06:44 +0100

    Lê múltiplas receitas do ficheiro "receitas.txt"

--- src/model/Recipes.java
+++ src/model/Recipes.java
@@ -38,7 +38,7 @@ public class Recipes {
         }
     }
 
-    public String toString(){//ola
+    public String toString(){
         String out = "";
         int c = 1;
         for(String[] str : recipes){
